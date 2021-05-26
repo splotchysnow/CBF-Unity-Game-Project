f5b57c003b11dadd06eefdc9e73db89cf10c1e09
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2021-05-26 16:01:52 -0700

    GUI updated

---
+++ Assets/GUI_Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GUI_Script : MonoBehaviour
+{
+
+
+
+    public void onClickStart()
+    {
+        SceneManager.LoadScene("Scene_01");
+    }
+    public void exitGame()
+    {
+        print("Game is ended.");
+        Application.Quit();
+    }
+}
